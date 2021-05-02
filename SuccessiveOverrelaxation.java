11f645f1fbc27429b91e6b692e9987d65d963989
aptung <70546542+aptung@users.noreply.github.com>
2021-05-01 20:31:01 -0700

    Added timing and fixed some of the infinity/NaN problems

--- SuccessiveOverrelaxation.java
+++ SuccessiveOverrelaxation.java
@@ -1,30 +1,43 @@
 
 public class SuccessiveOverrelaxation {
-	static final double THRESHOLD = 0.0000001;
+	static final double THRESHOLD = (double) 0.0000001;
 	
 	public static void main (String args[]) {
-		double[][] testA = {{4, -1, -6, 0}, 
+		double[][] testA = {{4, -1, -6, 0},
 							{-5, -4, 10, 8},
 							{0, 9, 4, -2},
 							{1, 0, -7, 5}};
+		printArr(multiplyMatrix(testA, new double[] {3, -2, 2, 1}));
 		double[] testB = {2, 21, -12, -6};
-		int bestIters = 10000;
-		double bestOmega = 0;
-		for (int i=0; i<472; i++) {
-			double omega = 0.1 + 0.001*i;
+		printArr(sor(testA, testB, 0.5)[0]);
+		int bestIters = 1000000000;
+		double bestTime = 1000000000;
+		double bestOmegaIters = 0;
+		double bestOmegaTime = 0;
+		for (int i=1; i<570; i++) {
+			double omega = 0.001*i;
+			double startTime = System.nanoTime();
 			double[][] result = sor(testA, testB, omega);
-			// System.out.print("omega = " + omega + " required " + result[1][0] + " iterations and returned ");
-			// printArr(result[0]);
+			double endTime = System.nanoTime();
+			 System.out.print("omega = " + omega + " required " + result[1][0] + " iterations and returned ");
+			 printArr(result[0]);
 			for (int j=0; j<result[1][0]/10; j++) {
 				System.out.print('*');
 			}
 			System.out.println();
 			if (result[1][0]<=bestIters) {
 				bestIters = (int) result[1][0];
-				bestOmega = omega;
+				bestOmegaIters = omega;
+			}
+			if (endTime-startTime<bestTime) {
+				bestTime = endTime - startTime;
+				bestOmegaTime = omega;
 			}
 		}
-		System.out.println("Best omega is " + bestOmega + " which takes " + bestIters + " iterations");
+		System.out.println("Solution: ");
+		printArr(sor(testA, testB, bestOmegaIters)[0]);
+		System.out.println("Best omega is " + bestOmegaIters + " which takes " + bestIters + " iterations");
+		System.out.println("Fastest omega is " + bestOmegaTime + " which takes " + bestTime + " nanoseconds");
 	}
 	
 	public static double[][] sor (double[][] A, double[] b, double omega) {
