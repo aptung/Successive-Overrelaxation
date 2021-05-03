1a6ee16ff2f872fa38529d528c095fea7a0170bf
aptung <70546542+aptung@users.noreply.github.com>
2021-05-03 11:41:41 -0700

    deleted print method

--- SuccessiveOverrelaxation.java
+++ SuccessiveOverrelaxation.java
@@ -108,12 +108,4 @@ public class SuccessiveOverrelaxation {
 		System.out.println();
 	}
 	
-	public static void printTwoDArr (double[][] arr) {
-		for (int i=0; i<arr.length; i++) {
-			for (int j=0; j<arr[0].length; j++) {
-				System.out.print(arr[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
 }
